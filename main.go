466445c51b389e47b38426b9950155eb78388c1e
Rayyan <rayyanaslam16@gmail.com>
2024-09-16 18:54:26 +0530

    Add fiber

--- main.go
+++ main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	// var name = "John"
+	// const name2 = "Doe"
+
+	// fmt.Println(name)
+	// fmt.Println(name2)
+
+	// name3 := "Blue Eyes White Dragon"
+	// fmt.Println(name3)
 }
