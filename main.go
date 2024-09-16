b98dcefdc58e47e7b29e043259e0360ef9e1137b
Rayyan <rayyanaslam16@gmail.com>
2024-09-16 23:45:49 +0530

    create endpoint for post

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Text      string `json:"text"`
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 
@@ -21,9 +27,28 @@ func main() {
 
 	app := fiber.New()
 
+	todos := []Todo{}
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		// return c.SendString("Hello, World!")
 		return c.Status(200).JSON(fiber.Map{"msg": "Blue Eyes White Dragon!"})
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": err.Error()})
+		}
+
+		if todo.Text == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Text is required"})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todos)
 	})
 
 	log.Fatal(app.Listen(":3000"))
